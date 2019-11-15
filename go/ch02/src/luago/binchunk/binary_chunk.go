38e065b136cf8872e3e0b2c34977fe3480e43630
Stackingrule <messageswitching@gmail.com>
2019-11-15 22:23:50 +0800

    add ch02

--- go/ch02/src/luago/binchunk/binary_chunk.go
+++ go/ch02/src/luago/binchunk/binary_chunk.go
@@ -20,3 +20,17 @@ type header struct {
 	luacNum			float64
 }
 
+
+const (
+	LUA_SIGNATURE		= "\x1bLua"
+	LUAC_VERSION		= 0x53
+	LUAC_FORMAT 		= 0
+	LUAC_DATA			= "\x10\x93\r\n\xla\n"
+	CINT_SIZE			= 4
+	CSIZET_SIZE			= 8
+	INSTRUCTION_SIZE	= 4
+	LUA_INTEGER_SIZE	= 8
+	LUA_NUMBER_SIZE		= 8
+	LUAC_INT			= 0x5678
+	LUAC_NUM			= 370.5
+)
